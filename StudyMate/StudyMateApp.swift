940a5430e061c71482120651c682e1281320aaa3
Maddie Adair <98988491+maddieadair@users.noreply.github.com>
2024-10-12 22:42:37 -0500

    Initial Commit

---
+++ StudyMate/StudyMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyMateApp.swift
+//  StudyMate
+//
+//  Created by Maddie Adair on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
